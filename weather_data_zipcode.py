2dd0ca98191454e79efb687eb43a794dcc992c24
AsisNath <nathasiskumar@yahoo.com>
2018-07-26 14:27:03 -0400

    Update weather_data_zipcode.py

--- weather_data_zipcode.py
+++ weather_data_zipcode.py
@@ -10,7 +10,6 @@ print("  /   |        |   |      | ")
 print(" /    |   _____|   |  ____| \n")
 
 import requests 
-
 #api_address_openweather = 'http://api.openweathermap.org/data/2.5/weather?=a371e67e3caa0ab622264a1323fa983f&zip='
 api_address_openweather = 'http://api.openweathermap.org/data/2.5/weather?zip='
 user_zip =input ('Enter your zipcode: ')
@@ -18,7 +17,6 @@ usa_city = (',us')
 api_key = '&appid=a371e67e3caa0ab622264a1323fa983f'
 url = api_address_openweather+user_zip+usa_city+api_key
 json_formate = requests.get(url).json()
-
 print('weather')
 json_formate_ind = json_formate['weather'][0]['main']
 print(json_formate_ind)
@@ -28,5 +26,4 @@ print('wind speed')
 print (json_formate['wind']['speed'])
 print('clouds')
 print (json_formate['clouds']['all'])
-
 print(json_formate)
